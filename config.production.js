5c3d74088e66e9f75c7d47423a2a53fe42e42378
ecossistema-renova <renovaecossistema@gmail.com>
2026-09-18 15:40:18 -0300

    prod: add Mercado Pago public checkout configuration

--- config.production.js
+++ config.production.js
@@ -2,5 +2,6 @@ window.OYAG_CONFIG=Object.freeze({
   environment:"production",
   supabaseUrl:"https://epbhiygonpkzlmbbsyqv.supabase.co",
   supabasePublishableKey:"sb_publishable_W6PD1uacSUSuBYjHSysdHQ_6fFi_Tur",
+  mercadoPagoPublicKey:"APP_USR-b769006b-1643-4bbc-aa1e-9034cc61d715",
   production:true
 });
