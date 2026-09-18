c9ce809c5c7553e6cdf1fc8e237147b937dd0a50
ecossistema-renova <renovaecossistema@gmail.com>
2026-09-18 14:55:19 -0300

    prod: remove obsolete homologation config

--- config.homolog.js
+++
@@ -1,6 +0,0 @@
-window.OYAG_CONFIG=Object.freeze({
-  environment:"production",
-  supabaseUrl:"https://epbhiygonpkzlmbbsyqv.supabase.co",
-  supabasePublishableKey:"sb_publishable_W6PD1uacSUSuBYjHSysdHQ_6fFi_Tur",
-  production:true
-});
