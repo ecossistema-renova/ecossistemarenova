4ae71fc3b9b4dae2b329b9624f03e844b7f0e780
ecossistema-renova <renovaecossistema@gmail.com>
2026-09-18 14:50:59 -0300

    prod: add canonical OYAG production config

---
+++ config.production.js
@@ -0,0 +1,6 @@
+window.OYAG_CONFIG=Object.freeze({
+  environment:"production",
+  supabaseUrl:"https://epbhiygonpkzlmbbsyqv.supabase.co",
+  supabasePublishableKey:"sb_publishable_W6PD1uacSUSuBYjHSysdHQ_6fFi_Tur",
+  production:true
+});
