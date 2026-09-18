9aacc3d93e6ca38e0f83b939b7402a82e116d472
ecossistema-renova <renovaecossistema@gmail.com>
2026-09-18 14:55:25 -0300

    prod: remove obsolete Mercado Pago sandbox checkout

--- mp-test/config.js
+++
@@ -1,5 +0,0 @@
-window.OYAG_MP_TEST_CONFIG=Object.freeze({
-  publicKey:"APP_USR-ec2eeabc-0095-43c0-b55a-1a5b455ea9ad",
-  supabaseUrl:"https://epbhiygonpkzlmbbsyqv.supabase.co",
-  environment:"test"
-});
